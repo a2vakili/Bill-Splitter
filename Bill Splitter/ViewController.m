453dc17b16a5921008a2ccd7e72720cdcb5d5cbc
Arsalan Vakili <arsenal.v.001@gmail.com>
2015-06-22 09:21:50 -0400

    second commit

--- Bill Splitter/ViewController.m
+++ Bill Splitter/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *totalBillEntered;
+@property (weak, nonatomic) IBOutlet UISlider *rangeOfPeople;
+@property (weak, nonatomic) IBOutlet UILabel *amountOfBillForEachPerson;
 
 @end
 
@@ -23,5 +26,31 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)calculateSplitAmount:(id)sender {
+    
+    NSNumberFormatter *currencyFormat= [[NSNumberFormatter alloc ]init];
+    currencyFormat.numberStyle= NSNumberFormatterDecimalStyle;
+    
+    NSNumber *billtotal=[currencyFormat numberFromString:self.totalBillEntered.text];
+    
+    if (billtotal == nil ) {
+        self.amountOfBillForEachPerson.text= @"Please enter a valid number";
+    }
+    else {
+        double billAmountEntered= [billtotal doubleValue];
+        int numberOfPeople= self.rangeOfPeople.value;
+        double amountPerPerson= billAmountEntered / numberOfPeople;
+        NSString *amountInString= [NSString stringWithFormat:@"$%0.1f", amountPerPerson];
+        self.amountOfBillForEachPerson.text= amountInString;
+    }
+    
+}
+
+-(void) touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event{
+    [self.view endEditing:YES];
+}
+
+
+
 
 @end
